538bce935c8663357cb42787bb7cffeedbfb33a7
Qpica <cs.k.kenez@gmail.com>
2022-10-07 19:33:49 +0200

    Backend init app created

---
+++ Backend/src/main/java/com/Biztonsagok/CAFFShop/CaffShopApplication.java
@@ -0,0 +1,13 @@
+package com.Biztonsagok.CAFFShop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaffShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CaffShopApplication.class, args);
+	}
+
+}
---
+++ Backend/src/test/java/com/Biztonsagok/CAFFShop/CaffShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Biztonsagok.CAFFShop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaffShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
